24c1f27077e9cdf8361829d2f87829f4bc8c750d
Teylor Chapman <5535@holbertonstudents.com>
2023-06-12 19:38:49 -0500

    Coined

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+	public Vector3 cameraOffset;
+
+	void LateUpdate ()
+	{
+		if (player != null)
+		{
+			transform.position = player.transform.position + cameraOffset;
+		}
+	}
+}
