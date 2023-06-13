b5ca01e20e63136ce8d2e6c262c948768db4977f
Teylor Chapman <5535@holbertonstudents.com>
2023-06-12 19:19:57 -0500

    come on fatso bust a move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 2f;
+
+	private Rigidbody playerRigidBody;
+	void Start ()
+	{
+		playerRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+		playerRigidBody.velocity = movement * speed;
+	}
+}
